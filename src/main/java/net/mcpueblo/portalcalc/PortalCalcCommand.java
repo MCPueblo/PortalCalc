078d2cb94c20d528ee0fb43554af018961ed1f31
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-01 14:43:41 -0400

    First attempt at sending coordinate thru a command

--- src/main/java/net/mcpueblo/portalcalc/PortalCalcCommand.java
+++ src/main/java/net/mcpueblo/portalcalc/PortalCalcCommand.java
@@ -1,5 +1,6 @@
 package net.mcpueblo.portalcalc;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,11 +11,16 @@ public class PortalCalcCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         String cmdName = cmd.getName().toLowerCase();
+        Player player = (Player) sender;
+        Location loc = player.getLocation();
+        int x = loc.getBlockX();
+        int y = loc.getBlockY();
+        int z = loc.getBlockZ();
 
         if (!cmdName.equals("portalcalc")) {
             return false;
         }
-        sender.sendMessage("shit works");
+        sender.sendMessage("You are at X: " + x + " Y: " + y + " Z: " + z);
 
         return true;
     }
