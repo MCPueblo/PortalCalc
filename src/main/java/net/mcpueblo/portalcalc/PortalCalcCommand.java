70b023c68c1ee387048172f59cd93b94118cd283
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-02 19:52:59 -0400

    Add config support

--- src/main/java/net/mcpueblo/portalcalc/PortalCalcCommand.java
+++ src/main/java/net/mcpueblo/portalcalc/PortalCalcCommand.java
@@ -10,8 +10,33 @@ import org.bukkit.entity.Player;
 
 public class PortalCalcCommand implements CommandExecutor {
 
+    PortalCalc plugin;
+
+    public PortalCalcCommand(PortalCalc instance) {
+        plugin = instance;
+    }
+
+    private ChatColor getCoordColor() {
+        String colorName = plugin.getConfig().getString("coord-color", null);
+
+        if (colorName == null) {
+            return ChatColor.WHITE;
+        }
+        if (colorName.isEmpty()) {
+            return null;
+        }
+
+        return ChatColor.getByChar(colorName);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        String calcMessage = plugin.getConfig().getString("calc-message");
+        String resultMessage = plugin.getConfig().getString("result-message");
+        String wrongDimensionError = plugin.getConfig().getString("wrong-dimension-error");
+        String coordColor = plugin.getConfig().getString("coord-color");
+
         if (cmd.getName().equalsIgnoreCase("portalcalc") && sender instanceof Player) {
             Player player = (Player) sender;
             Location loc = player.getLocation();
@@ -22,21 +47,33 @@ public class PortalCalcCommand implements CommandExecutor {
             if (player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
                 int portalx = x / 8;
                 int portalz = z / 8;
+                String netherCalcMessage = calcMessage.replace("<dimension>", "nether");
+                String netherResultMessage = resultMessage.replace("<dimension>", "nether");
+                String calcCoords = "X: " + x + " Y: " + y + " Z: " + z;
+                String resultCoords = "X: " + portalx + " Y: " + y + " Z: " + portalz;
 
-                sender.sendMessage(ChatColor.GOLD + "Recommended portal location for " + "X: " + x + " Y: " + y + " Z: " + z + " in the nether:");
-                sender.sendMessage(ChatColor.GOLD + "X: " + portalx + " Y: " + y + " Z: " + portalz);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', netherCalcMessage));
+                sender.sendMessage(getCoordColor() + calcCoords);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', netherResultMessage));
+                sender.sendMessage(getCoordColor() + resultCoords);
 
                 return true;
             } else if (player.getWorld().getEnvironment().equals(World.Environment.NETHER)) {
                 int portalx = x * 8;
                 int portalz = z * 8;
+                String overworldCalcMessage = calcMessage.replace("<dimension>", "overworld");
+                String overworldResultMessage = resultMessage.replace("<dimension>", "overworld");
+                String calcCoords = "X: " + x + " Y: " + y + " Z: " + z;
+                String resultCoords = "X: " + portalx + " Y: " + y + " Z: " + portalz;
 
-                sender.sendMessage(ChatColor.GOLD + "Recommended portal location for " + "X: " + x + " Y: " + y + " Z: " + z + " in the overworld:");
-                sender.sendMessage(ChatColor.GOLD + "X: " + portalx + " Y: " + y + " Z: " + portalz);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', coordColor + overworldCalcMessage));
+                sender.sendMessage(getCoordColor() + calcCoords);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', coordColor + overworldResultMessage));
+                sender.sendMessage(getCoordColor() + resultCoords);
 
                 return true;
             } else {
-                sender.sendMessage(ChatColor.RED + "You cannot create nether portals in this world!");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', wrongDimensionError));
 
                 return true;
             }
