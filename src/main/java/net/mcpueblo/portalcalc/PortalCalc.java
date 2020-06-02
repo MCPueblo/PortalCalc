fa7c09238d7bb2de2f29c25c8ec96f14aa097507
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-02 19:52:31 -0400

    Add config support

--- src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
+++ src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
@@ -1,17 +1,17 @@
 package net.mcpueblo.portalcalc;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PortalCalc extends JavaPlugin {
+public final class PortalCalc extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        /* Plugin startup logic */
-        getCommand("portalcalc").setExecutor(new PortalCalcCommand());
+        this.saveDefaultConfig();
+        getCommand("portalcalc").setExecutor(new PortalCalcCommand(this));
     }
 
     @Override
     public void onDisable() {
-        /* Plugin shutdown logic */
     }
 }
