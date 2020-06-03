11b26120ecba5ba7e3d9cd47e479cc35a103432b
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-02 22:34:38 -0400

    Removed unused import

--- src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
+++ src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
@@ -1,6 +1,5 @@
 package net.mcpueblo.portalcalc;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PortalCalc extends JavaPlugin {
