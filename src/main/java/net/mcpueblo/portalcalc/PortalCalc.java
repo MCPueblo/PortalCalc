1c052ee7ee069d8c7604ea316e5982114a5d329c
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-03 19:27:01 -0400

    Add reload command and log messages

--- src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
+++ src/main/java/net/mcpueblo/portalcalc/PortalCalc.java
@@ -1,7 +1,10 @@
 package net.mcpueblo.portalcalc;
 
+import net.mcpueblo.portalcalc.cmd.PortalCalcReload;
 import org.bukkit.plugin.java.JavaPlugin;
 import net.mcpueblo.portalcalc.cmd.PortalCalcCommand;
+
+import static java.util.logging.Level.INFO;
 
 public final class PortalCalc extends JavaPlugin {
 
@@ -9,8 +12,11 @@ public final class PortalCalc extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
         getCommand("portalcalc").setExecutor(new PortalCalcCommand(this));
+        getCommand("portalcalcreload").setExecutor(new PortalCalcReload(this));
+        getLogger().log(INFO,"Made for MCPueblo");
     }
     @Override
     public void onDisable() {
+        getLogger().log(INFO,"Stopping...");
     }
 }
